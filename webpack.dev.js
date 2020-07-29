a3937ff07ec3c0d3b07257fc91be90e785fe6705
Olivier Raimbaud <olivier.raimbaud@gmail.com>
2020-07-29 22:02:55 +0200

    deleted docs folder

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'docs'),
+    contentBase: path.resolve(__dirname, 'dist'),
     port: port
   },
   plugins: [
